3e7705438ca3ec14983a8cb3a8f1feb1fa78f46e
ingridhm <ingrd.hm@gmail.com>
2021-01-17 02:38:15 -0600

    Interfaz Configuracion

--- Proyecto (Festividades)/ConfiguracionViewController.swift
+++ Proyecto (Festividades)/ConfiguracionViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ConfiguracionViewController: UIViewController {
     
+    @IBOutlet weak var ConfiguracionView: UIView!
+    @IBOutlet weak var CorreoField: UITextField!
+    @IBOutlet weak var AnteriorField: UITextField!
+    @IBOutlet weak var NuevaField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        ConfiguracionView.backgroundColor = UIColor.white.withAlphaComponent(0.8)
+        ConfiguracionView.layer.cornerRadius = 10
     }
     
+    @IBAction func GuardarConfiguracion(_ sender: Any) {
+    }
 }
